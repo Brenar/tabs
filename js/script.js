24c92c72b82a117364252ebacb0d816371581379
Brenar <brenar123@gmail.com>
2019-07-04 12:06:49 +0300

    add timer

--- js/script.js
+++ js/script.js
@@ -34,4 +34,57 @@ window.addEventListener('DOMContentLoaded', function() {
             }
 
         });
+
+//таймер
+
+        let deadline = '2019-07-05';
+
+        function getTimeRemaining(endtime) {
+            let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };  
+    }
+
+    function setClock(id, endtime){
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = document.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+            else if (t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            }
+            else if (t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            } 
+            else if (t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            }
+        }
+
+
+
+    }
+    setClock('timer', deadline);
 });
